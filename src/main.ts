f0bbe5b17b87e699f45e71f85d13f33a9d1e912f
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-11-04 12:43:37 +0100

    Added tylumság

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
 import { Tree } from "./tree";
 
-console.log('fika')
+let tree : Tree = new Tree(20);
+
+let tree2 : Tree = new Tree(20);
+
+console.log(tree.Equals(tree2));
+
+
+console.log('Hello, world!')
--- src/tree.ts
+++ src/tree.ts
@@ -1,5 +1,65 @@
 class Tree {
 
+    private value!: number;
+
+    private left: Tree | null;
+
+    private right: Tree | null;
+
+    private set Value(v: number) {
+        if (v <= 0) {
+            throw new Error("value must be positive");
+        }
+        this.value = v;
+    }
+
+    private get Depth() : number {
+        
+        let leftDepth = this.left?.Depth || 0;
+        let rightDepth = this.right?.Depth || 0;
+        return (leftDepth < rightDepth ? leftDepth : rightDepth) + 1;
+    }
+
+    public constructor(value: number) {
+        this.Value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    public get Value() : number {
+        return this.value;
+    }
+
+    public Add(v: number): void {
+        if (!this.left)
+            this.left = new Tree(v);
+        else if (!this.right)
+            this.right = new Tree(v);
+        else if (this.left.Depth <= this.right.Depth)
+            this.left.Add(v);
+        else
+            this.right.Add(v);
+    }
+
+    public Display(d: number = 0) : void {
+        if (this.left) {
+            this.left.Display(d + 1);            
+        }
+        for (let i = d; i > 0; i--) {
+            console.log(' ');
+        }
+        console.log(this.value);
+        if (this.right) {
+            this.right.Display(d + 1);
+        }    
+    }
+
+    public Equals(otherTree: Tree) : boolean {
+        if (!otherTree) {
+            return false;
+        }
+        return this.value == otherTree.value;
+    }
 }
 
 export { Tree }
